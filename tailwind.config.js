fd80ab8d4c1a823543d33c53cb74e15f88e87233
ardane-s <komangaris2910@gmail.com>
2025-11-21 20:00:22 +0800

    Disable Tailwind purging completely - include all classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,21 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-/** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-        './app/**/*.php',
-    ],
+    content: {
+        files: [
+            './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+            './storage/framework/views/*.php',
+            './resources/**/*.blade.php',
+            './resources/**/*.js',
+            './resources/**/*.vue',
+            './app/**/*.php',
+        ],
+    },
 
     safelist: [
         {
-            pattern: /(bg|text|border)-(primary|secondary|neutral)-(50|100|200|300|400|500|600|700|800|900)/,
-        },
-        {
-            pattern: /(bg|text|border)-(orange|green|red|blue|yellow|indigo)-(50|100|200|300|400|500|600|700|800|900)/,
+            pattern: /.*/,  // Include ALL classes - no purging
         },
     ],
 
