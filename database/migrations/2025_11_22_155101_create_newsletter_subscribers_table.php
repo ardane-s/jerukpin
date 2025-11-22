24d2660294aeef8aae2f888db7d30471a6510dc1
ardane-s <komangaris2910@gmail.com>
2025-11-22 17:14:59 +0800

    Add migration files for avatar and newsletter subscribers

--- database/migrations/2025_11_22_155101_create_newsletter_subscribers_table.php
+++ database/migrations/2025_11_22_155101_create_newsletter_subscribers_table.php
@@ -6,20 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('newsletter_subscribers', function (Blueprint $table) {
             $table->id();
+            $table->string('email')->unique();
+            $table->timestamp('subscribed_at');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('newsletter_subscribers');
